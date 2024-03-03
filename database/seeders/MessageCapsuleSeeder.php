8644ab3a383fd582ae4f31d6da2b3027b372cd86
Chsaleem31 <devsaleemch@gmail.com>
2024-03-03 10:12:59 -0800

    MessageCapsule::Updated seeder and factory

--- database/seeders/MessageCapsuleSeeder.php
+++ database/seeders/MessageCapsuleSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\{MessageCapsule, User};
 
 class MessageCapsuleSeeder extends Seeder
 {
@@ -13,6 +13,6 @@ class MessageCapsuleSeeder extends Seeder
      */
     public function run(): void
     {
-        MessageCapsule::factory()->count(8)->recycle(User::factory()->create());
+        MessageCapsule::factory()->count(8)->recycle(User::factory()->create())->create();
     }
 }
